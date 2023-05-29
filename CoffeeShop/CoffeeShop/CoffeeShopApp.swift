cbae25da81f6c70915c1a93934d3187d55a66e96
witoldmetel <witoldmetel@gmail.com>
2023-05-29 20:24:19 +0200

    init

---
+++ CoffeeShop/CoffeeShop/CoffeeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeShopApp.swift
+//  CoffeeShop
+//
+//  Created by Witold on 29/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
