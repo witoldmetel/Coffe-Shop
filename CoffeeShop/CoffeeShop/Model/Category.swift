089f30939d443f05e00793465d1d5c999b475613
witoldmetel <witoldmetel@gmail.com>
2023-05-31 08:22:43 +0200

    add real data

--- CoffeeShop/CoffeeShop/Model/Category.swift
+++ CoffeeShop/CoffeeShop/Model/Category.swift
@@ -7,7 +7,10 @@
 
 import Foundation
 
-struct Category: Decodable {
+struct Category: Decodable, Identifiable {
+    var id: String {
+        return self.name
+    }
     var name: String
     var products: [Product] = []
 }
